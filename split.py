ca1ebaba3b6bd2f44b88070023553b3b0ee876f9
Hugo Corra <hugocorra@gmail.com>
2017-08-21 13:36:59 -0300

    moved songs list to a file

--- split.py
+++ split.py
@@ -5,40 +5,20 @@ import sys
 from pydub import AudioSegment
 
 
-def get_audio_time_tracks():
-    return [
-        ('1:20', 'Song with no name'),
-        ('3:34', 'Koto'),
-        ('4:52', 'How Did I Get Here'),
-        ('7:06', 'Big Girls Cry (ODESZA Remix)'),
-        ('10:28', 'White Lies'),
-        ('12:00', 'All We Need'),
-        ('14:25', 'Today'),
-        ('18:10', 'Saola (ODESZA Remix)'),
-        ('20:52', 'Above The Middle'),
-        ('23:06', 'Sun Models'),
-        ('26:53', 'If There\'s Time'),
-        ('28:30', 'One Day They\'ll Know (ODESZA Remix)'),
-        ('30:32', 'Open Wound (ODESZA Remix)'),
-        ('33:45', 'Keep Her Close'),
-        ('36:28', 'Bloom'),
-        ('39:09', 'Waited 4 U (ODESZA Remix)'),
-        ('43:20', 'Divinity (ODESZA Remix)'),
-        ('46:10', 'My Friends Never Die'),
-        ('48:42', 'Kusanagi'),
-        ('50:57', 'Honestly no idea'),
-        ('52:34', 'Memories That You Call'),
-        ('56:27', 'Faded (ODESZA Remix)'),
-        ('1:00:00', 'Something About You (ODESZA Remix)'),
-        ('1:04:26', 'Say My Name'),
-        ('1:08:43', 'IPlayYouListen'),
-        ('1:11:52', 'A Heartbreak (ODESZA Remix)'),
-        ('1:16:09', 'Make Me Feel Better'),
-    ]
+def get_audio_time_tracks(songsfile):
+    songs = []
 
+    with open(songsfile) as f:
+        for l in f:
+            time, name = l.split(' ', 1)
+            songs.append((time, name.rstrip()))
+
+    return songs
 
 def time_to_milliseconds(lst_tracks):
     lreturn = []
+
+    print lst_tracks
 
     for track in lst_tracks:
         track_time = track[0].split(':')
@@ -66,11 +46,16 @@ if __name__ == '__main__':
         print 'file to be splitted must be provided.'
         exit(1)
 
+    if len(sys.argv) > 0:
+        songs_file = sys.argv[2]
+    else:
+        print 'songs file must be provided.'
+        exit(1)
 
     sound = AudioSegment.from_file(filename + file_extension, file_extension[1:])
 
     print 'converting time'
-    tracks = get_audio_time_tracks()
+    tracks = get_audio_time_tracks(songs_file)
     tracks_ms = time_to_milliseconds(tracks)
 
     for i in  xrange(0, len(tracks_ms)):
